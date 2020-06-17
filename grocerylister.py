bd07ca967a60ad9f79375d2c31e11dc22dba785f
Syris Norelli <snore001@ucr.edu>
2020-06-17 12:27:46 -0700

    Remove irrelevant files, minor sketch work on grocerylister

--- grocerylister.py
+++ grocerylister.py
@@ -1,30 +1,49 @@
+# TODO: come up with a rules system that makes sense
+# (dictionary lookup by item, then move to "all" group)
+
 class Ingredient:
-    def __init__(self, name='', quantity=0, qtype=''):
+    def __init__(self, name='', quantity=0, qtype='', liquid=False):
         # qtype examples: "cup", "fl oz", "tbsp"
         self.name = name
         self.quantity = quantity
         self.qtype = qtype
+        self.liquid = liquid
+    def _convert(self, other):
+        # Returns 
+        assert type(other) == Ingredient, 
+            (f'Can only do operations between two Ingredient objects.' 
+             + f'Right obj ({other}) is not of Ingredient class.')
+        assert self.name == other.name, 
+            ( f'Cannot do operations between two different Ingredient objects:'
+              + f'({self.name}) AND ({other.name})')
+
+        if self.qtype == other.qtype:
+            return
+        else: # Unit convert
+
+
     def __add__(self,other):
         # Proper usage assertions
-        assert type(other) == Ingredient, 'Can only add together two Ingredient objects'
-        assert self.name == other.name, 'Cannot add together two different ingredients: (' + repr(self.name) + ') AND (' + repr(other.name) + ')'
         
         if self.qtype == other.qtype: # Easy!
             return Ingredient(self.name,self.quantity+other.quantity,self.qtype)
+        
         else: # Harder. Needs unit conversion.
-            conversions = {(self.qtype,other.qtype),(other.qtype,self.qtype)} # So you can get both directions (tbsp -> tsp) and (tsp -> tbsp)
-            for rule in addrules:
-                if rule[0] in conversions and ('any' in rule[2]  or  self.name in rule[2]): # Correct conversion (tbsp -> tsp) and allowed for type of ingredient
-                    relevant_rule = rule
-                    break
-            else: # Runs only if no conversion was found
-                raise Exception('Failed to add together (' + repr(self.name) + ') AND (' + repr(other.name) + ') due to quant type mismatch: (' + repr(self.qtype) + ') AND (' + repr(other.qtype) + ')')
+            raise NotImplementedError
+            # conversions = {(self.qtype,other.qtype),(other.qtype,self.qtype)} # So you can get both directions (tbsp -> tsp) and (tsp -> tbsp)
+            # for rule in addrules:
+            #     if rule[0] in conversions and ('any' in rule[2]  or  self.name in rule[2]): # Correct conversion (tbsp -> tsp) and allowed for type of ingredient
+            #         relevant_rule = rule
+            #         break
+            # else: # Runs only if no conversion was found
+            #     raise Exception('Failed to add together (' + repr(self.name) + ') AND (' + repr(other.name) + ') due to quant type mismatch: (' + repr(self.qtype) + ') AND (' + repr(other.qtype) + ')')
             
-            priority_qtype = [self.qtype,other.qtype].index(relevant_rule[0][0]) # For example, 'tbsp' is preferred in rule ('tbsp','tsp')
-            if priority_qtype == 0:
-                return Ingredient(self.name,self.quantity + other.quantity/relevant_rule[1], self.qtype)
-            else:
-                return Ingredient(self.name,other.quantity + self.quantity/relevant_rule[1], other.qtype)
+            # priority_qtype = [self.qtype,other.qtype].index(relevant_rule[0][0]) # For example, 'tbsp' is preferred in rule ('tbsp','tsp')
+            # if priority_qtype == 0:
+            #     return Ingredient(self.name,self.quantity + other.quantity/relevant_rule[1], self.qtype)
+            # else:
+            #     return Ingredient(self.name,other.quantity + self.quantity/relevant_rule[1], other.qtype)
+    
     def __truediv__(self, bottom): # The other one is __floordiv__
         ''' Returns a float ratio after doing correct unit conversion. Units cancel when dividing. '''
         assert type(bottom) == Ingredient, 'Can\'t divide by a non-Ingredient object'

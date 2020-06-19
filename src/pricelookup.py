292b3f0596c985cd15a08b1d6d850e66344eaafa
Syris Norelli <snore001@ucr.edu>
2020-06-19 15:33:03 -0700

    Sketch: Add IGNORE params for price computation

--- src/pricelookup.py
+++ src/pricelookup.py
@@ -7,6 +7,19 @@ def updatePrices(inglist):
 
     pass
 
+def computePrice(inglist):
+    # Actually does the computation and adds everything together
+    # Some ingredients are usually used in pinch quantities and don't
+    #     really make sense to compute. Or they might be on a non-instacart
+    #     site, which this code doesn't support. Here they are:
+    IGNORE = {
+        'sichuan peppercorns', # non-Instacart
+        'salt', # pinch
+        'pepper', # pinch
+        'black pepper', #pinch
+        'sea salt' # pinch
+    }
+
 if __name__ == "__main__":
 
     # Generate a list to work with
